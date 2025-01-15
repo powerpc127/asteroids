f0d903aa43ab2e6fb4308f4ba66c144f56299ad7
powerpc127 <crowellpeterm@gmail.com>
2025-01-15 17:30:56 -0600

    Added circle and player classes, implemented motionless player

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player1 = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         val = clock.tick(60)
         dt = val/1000
     print(f"Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
 
 if __name__ == "__main__":
     main()
